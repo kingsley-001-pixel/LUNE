f9aa2a0e09980395dd55459fd59be2a6ba6f88a5
kingsley-001-pixel <240056576+kingsley-001-pixel@users.noreply.github.com>
2026-03-01 19:11:54 +0100

    updated app.js CORS issue

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,14 +6,23 @@ import tmdbRoute from "./routes/tmdb.route.js"
 const app = express();
 
 app.use(cors({
-    origin: [
-    "http://localhost:5173",
-    "https://lune-9zy7.vercel.app"
-    ],
+    origin: (origin, callback) => {
+    const allowedOrigins = [
+      "http://localhost:5173",           // dev
+      "https://lune-9zy7.vercel.app"    // deployed frontend
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+    } else {
+        callback(new Error("Not allowed by CORS"));
+    }
+    },
     credentials: true,
     methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}))
+    allowedHeaders: ["Content-Type","Authorization"]
+}));
+
+app.options("/", cors());
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
