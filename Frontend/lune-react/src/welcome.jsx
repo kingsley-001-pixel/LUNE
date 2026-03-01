7997739e1668a2ed1357a9078832bf5c0b2795fa
kingsley-001-pixel <240056576+kingsley-001-pixel@users.noreply.github.com>
2026-03-01 07:43:15 +0100

    Updated welcome page

--- Frontend/lune-react/src/welcome.jsx
+++ Frontend/lune-react/src/welcome.jsx
@@ -6,11 +6,14 @@ function Welcome() {
     }, 5000);
     
     return(
-        <div className="bg-blue-700 text-white flex justify-center items-center h-screen">
-            <h1 className="text-9xl font-serif font-extrabold shadow-4xl shadow-white">Welcome to LUNE</h1>
+        <div className="bg-primary text-white block md:flex justify-center  text-center items-center h-screen">
+            <div className="absolute top-[30%] bottom-0 left-0 right-0">
+
+            <h1 className="text-6xl md:text-8xl md:mx-3 font-serif font-extrabold shadow-4xl shadow-white">Welcome to LUNE</h1>
             <br />
             <br />
-            <h2 className="text-6xl font-roboto font-bold">Home of Movies</h2>
+            <h2 className="text-4xl font-roboto font-bold">Home of Movies</h2>
+            </div>
 
         </div>
     )
